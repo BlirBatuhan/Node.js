6e05cbc1d6a80e8b5ba65a338d81a7de8878f89d
Batuhan Bilir <bth.01srp@gmail.com>
2024-04-25 19:10:30 +0300

    Controller ve Route eklendi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
 
 dotenv.config();
 
@@ -16,9 +17,12 @@ app.set('view engine','ejs');
 //static files middlewaare
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {res.render('index');});
+app.use('/',pageRoute);
 
-app.get('/about', (req, res) => {res.render('about');});
+
+/* app.get('/', (req, res) => {res.render('index');});
+
+app.get('/about', (req, res) => {res.render('about');}); */
 
 
 app.listen(port, () => {console.log(`Listening on ${port}`);});
---
+++ controller/pageController.js
@@ -0,0 +1,5 @@
+const getIndexPage = (req,res) => { res.render('index'); };
+
+const getAboutPage = (req,res) => { res.render('about'); }; 
+
+export {getAboutPage, getIndexPage};
