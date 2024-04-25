d7afd9ae15c7a1738c7879b7fba160fc30e900d1
Batuhan Bilir <bth.01srp@gmail.com>
2024-04-25 20:07:49 +0300

    Schema yapısı denendi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
 import pageRoute from './routes/pageRoute.js';
+import photoRoute from './routes/photoRoute.js';
 
 dotenv.config();
 
@@ -17,7 +18,9 @@ app.set('view engine','ejs');
 //static files middlewaare
 app.use(express.static('public'))
 
+//routes
 app.use('/',pageRoute);
+app.use('/photo',photoRoute);
 
 
 /* app.get('/', (req, res) => {res.render('index');});
---
+++ routes/photoRoute.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import * as photoController from '../controller/photoController.js';
+
+const router = express.Router();
+
+router.route("/").post(photoController.createPhoto);
+
+export default router;
