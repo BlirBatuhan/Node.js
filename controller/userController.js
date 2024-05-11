690dbbf1783d68be8a1cf39e254e12fede6d7e85
Batuhan Bilir <bth.01srp@gmail.com>
2024-05-11 17:54:10 +0300

    Login sayfasını ve işlemlerini halletik , şifreleri hashledik

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 const createUser = async (req,res) => {
     try {
@@ -9,4 +10,31 @@ const createUser = async (req,res) => {
     }
 };
 
-export {createUser};
+const loginUser = async (req,res) => {
+    try {
+        const {userName,password} = req.body;
+        const user = await User.findOne({userName});
+        let same = false;
+        if(user){
+            // Kullanıcı bulunduysa şifreleri karşılaştırıyoruz.
+            same = await bcrypt.compare(password,user.password);
+        }
+        else{
+            // Kullanıcı bulunamadıysa hata döndürüyoruz.
+           return res.status(401).json({succeded:false, error:"There is no user with that name"});
+        }
+
+        if(same){
+             // Şifre eşleşiyorsa giriş yapıyoruz.
+            res.status(200).send("you loggeend in")
+        }
+        else{
+            // Şifre eşleşmiyorsa hata döndürüyoruz.
+            res.status(401).json({succeded:false, error:"Wrong password"});
+        }
+    } catch (error) {
+        res.status(500).json({succeded:false, error});
+    }
+};
+
+export {createUser,loginUser};
--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import bcyrpt from "bcrypt";
 
 const { Schema } = mongoose;
-
+// kullanıcı modelini oluşturduk
 const userSchema = new Schema({
     userName: {
         type: String,
@@ -20,7 +21,19 @@ const userSchema = new Schema({
 }
 ,{
     timestamps: true
-})
+});
+
+//password'ü hashledik
+userSchema.pre("save",  function (next) {
+    const user = this
+    bcyrpt.hash(user.password,10,(err,hash) => {
+        if(err){
+            return next(err)
+        }
+        user.password =  hash
+        next()
+    })
+});
 
 const User = mongoose.model("User", userSchema);
 
