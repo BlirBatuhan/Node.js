d22dcf66ac0dbd2988457f15e4c72d4718a9c806
Batuhan Bilir <bth.01srp@gmail.com>
2024-07-30 19:31:58 +0300

    Görsel Yüklemek ve Cloudinary bağlantı kuruldu

--- controller/photoController.js
+++ controller/photoController.js
@@ -1,6 +1,18 @@
 import Photo from "../models/photoModel.js";
+import {v2 as cloudinary} from'cloudinary';
+import fs from 'fs';
 
 const createPhoto = async (req, res) => {
+
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+            use_filename: true,
+            folder: 'lenslight_tr',
+        }
+    )
+
+    console.log('RESULT::', result);
 
     const { name, description } = req.body;
 
@@ -13,7 +25,9 @@ const createPhoto = async (req, res) => {
             name,
             description,
             user: res.locals.user._id,
+            url: result.secure_url,
         });
+        fs.unlinkSync(req.files.image.tempFilePath); 
         res.status(201).redirect('/users/dashboard');
     } catch (error) {
         console.error(error); // Hataları daha iyi anlamak için
@@ -38,7 +52,7 @@ const getAllPhotos = async (req,res) => {
 
 const getAPhoto = async (req,res) => {
     try{
-        const photo = await Photo.findById({_id : req.params.id});
+        const photo = await Photo.findById({_id : req.params.id}).populate('user');
         res.status(200).render('photo', { 
         photo, 
         link: 'photo'
